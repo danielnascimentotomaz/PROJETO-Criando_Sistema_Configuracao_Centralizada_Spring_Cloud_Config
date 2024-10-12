d36e71add63c70f3aeecb989910fc028179337c3
danielnascimentotomaz <danielsabio1000@hotmail.com>
2024-10-12 09:36:00 -0300

    Criação do projeto config-server

---
+++ config-server/src/test/java/com/dio/me/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.me.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
